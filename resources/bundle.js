a9d7c51f66012ff8c6c977b09f21d389c8769216
Peter Mathis <peter.mathis@kombinat.at>
2024-10-25 11:29:48 +0200

    meta update and lint

--- resources/bundle.js
+++ resources/bundle.js
@@ -2,9 +2,8 @@ import registry from "@patternslib/patternslib/src/core/registry";
 
 import "./pat-related-images/related-images";
 
-// Register Fancybox gloablly
+// Register Fancybox globally
 async function register_global_libraries() {
-    // Register Fancybox globally
     const Fancybox = (await import("@fancyapps/ui")).Fancybox;
     window.Fancybox = Fancybox;
 }
